b6294d54a9a4e546d54bfbdff7ae0fcb875b2694
Informatica Arelux <informatica@arelux.com>
2020-03-31 15:10:48 +0200

    mail_arelux

---
+++ mail_arelux/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Mail Arelux',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base', 'mail', 'ont_base_mail'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,    
+}
---
+++ mail_arelux/models/mail_thread.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from openerp import api, models, fields
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class MailThread(models.AbstractModel):
+    _inherit = 'mail.thread'
+        
+    @api.multi
+    def _message_auto_subscribe_notify(self, partner_ids):
+        not_notify = False
+        
+        for record in self:
+            if record._name=='sale.order':
+                if record.opportunity_id.id!=False and record.opportunity_id.user_id.id==False:#Fix leads 3 emails (lead + 2 sale.order)
+                    not_notify = True
+            elif record._name=='account.invoice':
+                not_notify = True
+                                                                                                                 
+            if record._name=='crm.lead' and record.ar_qt_activity_type=='arelux':
+                not_notify = True
+            elif record._name=='sale.order' and record.ar_qt_activity_type=='arelux':
+                not_notify = True
+                                                                
+            if not_notify==False:            
+                return super(MailThread, self)._message_auto_subscribe_notify(partner_ids)
+            else:
+                return False
